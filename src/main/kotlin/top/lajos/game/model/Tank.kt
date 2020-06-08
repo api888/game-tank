249e8d8da6bdfde364671eab4612313c05cd05cb
lajos <lj@yunpankeji.com>
2020-06-08 16:56:06 +0800

    修改坦克模型

--- src/main/kotlin/top/lajos/game/model/Tank.kt
+++ src/main/kotlin/top/lajos/game/model/Tank.kt
@@ -2,16 +2,19 @@ package top.lajos.game.model
 
 import org.itheima.kotlin.game.core.Painter
 import top.lajos.game.Config
+import top.lajos.game.business.Attackable
 import top.lajos.game.business.Blockable
 import top.lajos.game.business.Movable
+import top.lajos.game.business.Sufferable
 import top.lajos.game.enums.Direction
 
 /**
  * 我方坦克
  *
  * 具备移动能力
+ * 具备阻挡能力
 */
-class Tank(override var x: Int, override var y: Int) : Movable {
+class Tank(override var x: Int, override var y: Int) : Movable, Blockable, Sufferable {
     override val width: Int = Config.block
     override val height: Int = Config.block
 
@@ -21,6 +24,8 @@ class Tank(override var x: Int, override var y: Int) : Movable {
     override val speed: Int = 8
     // 坦克不可以走的方向
     private var badDirection: Direction? = null
+    // 血量
+    override var blood: Int = 20
 
     override fun draw() {
         // 根据坦克的方向进行绘制
@@ -70,32 +75,33 @@ class Tank(override var x: Int, override var y: Int) : Movable {
     }
 
 
-    override fun willCollision(block: Blockable): Direction? {
-        // 未来的坐标
-        var x: Int = this.x
-        var y: Int = this.y
-        // 将要碰撞时做判断
-        when (currentDirection) {
-            Direction.UP -> y -= speed
-            Direction.DOWN -> y += speed
-            Direction.LEFT -> x -= speed
-            Direction.RIGHT -> x += speed
-        }
-        // 检测碰撞：检测下一步是否碰撞
-        val collision: Boolean = when {
-            block.y + block.height <= y -> { // 如果阻挡物 在运动物的上方 不碰撞
-                false
-            }
-            y + height <= block.y -> { // 如果阻挡物 在运动物的下方 不碰撞
-                false
-            }
-            block.x + block.width <= x -> { // 如果阻挡物 在运动物的左方 不碰撞
-                false
-            }
-            else -> x + width > block.x
-        }
-        return if (collision) currentDirection else null
-    }
+//    override fun willCollision(block: Blockable): Direction? {
+//        // 未来的坐标
+//        var x: Int = this.x
+//        var y: Int = this.y
+//        // 将要碰撞时做判断
+//        when (currentDirection) {
+//            Direction.UP -> y -= speed
+//            Direction.DOWN -> y += speed
+//            Direction.LEFT -> x -= speed
+//            Direction.RIGHT -> x += speed
+//        }
+//        // 检测碰撞：检测下一步是否碰撞
+////        val collision: Boolean = when {
+////            block.y + block.height <= y -> { // 如果阻挡物 在运动物的上方 不碰撞
+////                false
+////            }
+////            y + height <= block.y -> { // 如果阻挡物 在运动物的下方 不碰撞
+////                false
+////            }
+////            block.x + block.width <= x -> { // 如果阻挡物 在运动物的左方 不碰撞
+////                false
+////            }
+////            else -> x + width > block.x
+////        }
+//        val collision = checkCollision(block.x, block.y, block.width, block.height, x, y, width, height)
+//        return if (collision) currentDirection else null
+//    }
 
     override fun notifyCollision(direction: Direction?, block: Blockable?) {
         // TODO：接收到碰撞信息
@@ -107,7 +113,7 @@ class Tank(override var x: Int, override var y: Int) : Movable {
      */
     fun shot(): Bullet  {
 
-        return Bullet(currentDirection) { bulletWidth, bulletHeight ->
+        return Bullet(this, currentDirection) { bulletWidth, bulletHeight ->
             // 计算子弹真是的坐标
             // 如果坦克是向上的，计算子弹的位置：bulletX = tankX + (tankW - bulletW) / 2, bulletY = tankY - bulletH / 2
             var bulletX: Int = 0
@@ -132,6 +138,11 @@ class Tank(override var x: Int, override var y: Int) : Movable {
             }
             Pair(bulletX, bulletY)
         }
+    }
+
+    override fun notifySuffer(attackable: Attackable): Array<View>? {
+        blood -= attackable.attackPower
+        return arrayOf(Blast(x, y))
     }
 
 }
