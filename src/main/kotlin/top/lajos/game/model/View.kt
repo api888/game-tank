8957ec68ba1fe024793022ce3e445d7694f711cd
lajos <lj@yunpankeji.com>
2020-06-08 16:56:26 +0800

    添加单例模型

--- src/main/kotlin/top/lajos/game/model/View.kt
+++ src/main/kotlin/top/lajos/game/model/View.kt
@@ -13,4 +13,23 @@ interface View {
     val height: Int
     // 显示
     fun draw()
+    // 碰撞检测
+    fun checkCollision(x1: Int, y1: Int, w1: Int, h1: Int,
+                       x2: Int, y2: Int, w2: Int, h2: Int): Boolean {
+        // 两个物体的x, y, w, h的比较
+        // 检测碰撞：检测下一步是否碰撞
+        return when {
+            y2 + h2 <= y1 -> { // 如果阻挡物 在运动物的上方 不碰撞
+                false
+            }
+            y1 + h1 <= y2 -> { // 如果阻挡物 在运动物的下方 不碰撞
+                false
+            }
+            x2 +w2 <= x1 -> { // 如果阻挡物 在运动物的左方 不碰撞
+                false
+            }
+            else -> x1 + w1 > x2
+        }
+    }
+
 }
