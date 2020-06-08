2c74a40e604e0e0503e5270ad9f6dc92820b32c0
lajos <lj@yunpankeji.com>
2020-06-08 16:55:49 +0800

    修改铁墙模型

--- src/main/kotlin/top/lajos/game/model/Steel.kt
+++ src/main/kotlin/top/lajos/game/model/Steel.kt
@@ -2,14 +2,19 @@ package top.lajos.game.model
 
 import org.itheima.kotlin.game.core.Painter
 import top.lajos.game.Config
+import top.lajos.game.business.Attackable
 import top.lajos.game.business.Blockable
+import top.lajos.game.business.Sufferable
 
 /**
  *  铁墙
  *
  *  具有阻塞能力
  */
-class Steel(override val x: Int, override val y: Int) : Blockable {
+class Steel(override val x: Int, override val y: Int) : Blockable, Sufferable {
+    override val blood: Int = 5
+
+
     // 位置
 //    override val x: Int = 200
 //    override val y: Int = 200
@@ -20,4 +25,6 @@ class Steel(override val x: Int, override val y: Int) : Blockable {
     override fun draw() {
         Painter.drawImage("img/steel.gif", x, y)
     }
+
+    override fun notifySuffer(attackable: Attackable): Array<View>?  = null
 }
