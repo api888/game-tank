3482e18430f2083a2cfddcf33f88cbc4c8cdba59
lajos <lj@yunpankeji.com>
2020-06-08 16:51:25 +0800

    添加攻击能力的接口

--- src/main/kotlin/top/lajos/game/business/Attackable.kt
+++ src/main/kotlin/top/lajos/game/business/Attackable.kt
@@ -8,4 +8,15 @@ import top.lajos.game.model.View
 * */
 interface Attackable : View {
 
+    // 所有者
+    val owner: View
+
+    /**
+     * 攻击力
+     */
+    val attackPower: Int
+    //判断是否碰撞
+    fun isCollision(sufferable: Sufferable): Boolean
+
+    fun notifyAttack(sufferable: Sufferable)
 }
